04453fad4660f1589200cae237510a9f2caba287
zen.chong <lordzizzy@gmail.com>
2021-02-16 01:36:20 +0800

    add optimized bubble sort function

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,20 +1,53 @@
 # Bubble sort
-# Runtime: O(N^2) average and worst case
+# Runtime: O(N^2) average and worst case, best case O(N)
 # Memory: O(1)
+
+from typing import Callable, List
+from termcolor import colored
 
 import random
 
 
-def bubble_sort(numbers):
-    n = len(numbers)
+def bubble_sort_simple(nums: List[int]) -> None:
+    n = len(nums)
     for i in range(n - 1):
         for j in range(0, n - i - 1):
-            if numbers[j] > numbers[j + 1]:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+
+def bubble_sort_optimized(nums: List[int]) -> None:
+    n = len(nums) - 1
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(n):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                sorted = False
+        n -= 1
+
+
+SortFunc = Callable[[List[int]], None]
+
+
+def test_sort(func: SortFunc, nums: List[int], expected: List[int]):
+    original = list(nums)
+    func(nums)
+    if nums == expected:
+        print(
+            colored(f"PASSED {func.__name__} => {original} sorted is {nums}", "green")
+        )
+    else:
+        print(
+            colored(
+                f"PASSED {func.__name__} => {original} sorted is {nums}, but expected {expected}",
+                "red",
+            )
+        )
 
 
 if __name__ == "__main__":
-    random_numbers = [random.randrange(0, 99) for num in range(0, 10)]
-    print('list before sorting: ', random_numbers)
-    bubble_sort(random_numbers)
-    print('list after sorting: ', random_numbers)
+    random_numbers = [random.randrange(0, 99) for _ in range(0, 10)]
+    test_sort(bubble_sort_simple, random_numbers, sorted(random_numbers))
+    test_sort(bubble_sort_optimized, random_numbers, sorted(random_numbers))
